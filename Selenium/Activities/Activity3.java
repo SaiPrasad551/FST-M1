da6537df6f7cab9d8ebd4a02a9a94ff4b6afa0c9
SaiPrasad551 <Tota.Sai.Prasad@ibm.com>
2024-05-29 14:52:05 +0530

    Add files via upload

---
+++ Selenium/Activities/Activity3.java
@@ -0,0 +1,31 @@
+package Activities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Activity3
+{
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.firefoxdriver().setup();
+
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("https://v1.training-support.net/selenium/login-form");
+        System.out.println("Home page title: " + driver.getTitle());
+        Thread.sleep(2000);
+
+        driver.findElement(By.xpath("//input[@id='username']")).sendKeys("admin");
+
+        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("password");
+
+        driver.findElement(By.xpath("//button[text()='Log in']")).click();
+        Thread.sleep(2000);
+
+        String message = driver.findElement(By.xpath("//div[@id='action-confirmation']")).getText();
+        System.out.println("Login message: " + message);
+
+        driver.quit();
+    }
+}
